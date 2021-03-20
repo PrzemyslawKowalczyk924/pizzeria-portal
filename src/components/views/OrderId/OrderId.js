1cb915b30010da385b1738933c6ddce294aa8eee
unknown <kowalczyk.przemek@wp.pl>
2021-03-20 22:32:42 +0100

    Add identificators to id-with components

--- src/components/views/OrderId/OrderId.js
+++ src/components/views/OrderId/OrderId.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import styles from './OrderId.module.scss';
 
 const OrderId = () => (
   <div className={styles.component}> 
     <h2>OrderId View</h2>   
+    <Link to={`${process.env.PUBLIC_URL}/table`} activeClassName='active'>table</Link>
   </div>
 );
-  
+
 OrderId.propTypes = {
   children: PropTypes.node,
 };
